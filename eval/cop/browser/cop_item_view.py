6fe6c276233ec760fbb6e56814fae2dc296f94d7
glenn <glenn@glenn-Lenovo-G480>
2017-02-10 21:00:14 +0800

    added new functions

--- eval/cop/browser/cop_item_view.py
+++ eval/cop/browser/cop_item_view.py
@@ -9,4 +9,15 @@ class Index(dexterity.DisplayForm):
     grok.require('zope2.View')
     grok.template('cop_item_view')
     grok.name('view')
-
+    
+    def news_items(self):
+        catalog = self.context.portal_catalog
+        context = self.context
+        news = catalog.unrestrictedSearchResults(path={'query':'/'.join(context.getPhysicalPath()), 'depth':1}, portal_type='News Item', sort_order='reverse', sort_on='created')
+        return news
+    
+    def get_items(self, portal_type=None):
+        catalog = self.context.portal_catalog
+        context = self.context
+        brains = catalog.unrestrictedSearchResults(path={'query':'/'.join(context.getPhysicalPath()), 'depth':1}, portal_type=portal_type, sort_order='reverse', sort_on='created', sort_limit=5)[:5]
+        return brains
